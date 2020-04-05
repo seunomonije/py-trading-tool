66cda38e344c2d37a1c92d86e1ce2ff156950614
seunomonije <oseun22@gmail.com>
2020-04-04 19:06:32 -0500

    took out packages

--- README.md
+++
@@ -1,2 +0,0 @@
-## Python Trading Tool
-Simple trading tool I created to help me make trading decisions
--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+from packages import yfinance as yf
+
+#tickerSymbol = 'FB'
+#
+#tickerData = yf.Ticker(tickerSymbol)
+#
+#tickerDf = tickerData.history(period='1d', start='2010-1-1', end='2020-1-25')
+#
+#tickerDf
+
 print("Let's see if this works!")
